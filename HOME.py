e10f7cfd65b1659d5577eecf869f83a407d02636
MK <99416359+MK316@users.noreply.github.com>
2025-09-22 19:54:14 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.markdown("### ❄️ Phonetics Chapter 1 exercises")
 st.write("Exercises A, B-C, D, E, F, K, L")
+st.markdown("#### To get a pdf report, make sure you write your name in English.")
 st.caption("2025. 9. 22")
